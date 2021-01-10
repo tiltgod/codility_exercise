58aee3aa2010b3c744b0d2f5b7a6651247b537a1
tiltgod <59070084@it.kmitl.ac.th>
2021-01-10 10:51:21 +0700

    CyclicRotation but 100%

--- CyclicRotation.py
+++ CyclicRotation.py
@@ -2,6 +2,8 @@ from collections import deque
 
 def solution(A, K):
     # write your code in Python 3.6
+    if A == []:
+        return A
     Adeq = deque(A)
     for i in range(K):
         x = Adeq.pop()
