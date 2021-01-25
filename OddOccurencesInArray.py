6e92c3ca2eb7b199a3d93a1d3891d72e4e7d8186
tiltgod <59070084@it.kmitl.ac.th>
2021-01-25 10:22:52 +0700

    OddOccurrencesInArray

--- OddOccurencesInArray.py
+++ OddOccurencesInArray.py
@@ -1,9 +1,7 @@
 def solution(A):
     for i in range(len(A)):
-        ls = A.copy()
-        ls.pop(i)
-        if A[i] in ls:
-            pass
-        else:
-            return A[i] 
+        ans = 0
+        for i in A:
+            ans ^= i
+        return ans
         
